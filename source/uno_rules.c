c84c1bec5e50cbb02031d92c9c06cdfea44e8cf2
srpnsk <s04n02@gmail.com>
2025-12-14 22:26:49 +0300

    total refactoring...

--- source/uno_rules.c
+++ source/uno_rules.c
@@ -1,24 +1,5 @@
 #include "uno_rules.h"
 
-/*
- * Можно ли сыграть карту
- */
-bool can_play_card(Card player_card, Color current_color, Card top_card) {
-  if (player_card.color == COL_WILD)
-    return true;
-
-  if (player_card.color == current_color)
-    return true;
-
-  if (player_card.value == top_card.value)
-    return true;
-
-  return false;
-}
-
-/*
- * Индекс следующего игрока
- */
 int next_player_index(Game *game) {
   if (game->direction_clockwise) {
     return (game->current_player + 1) % game->players_count;
@@ -28,23 +9,13 @@ int next_player_index(Game *game) {
   }
 }
 
-/*
- * Применение эффектов карты
- */
 void apply_card_effect(Game *game, Card played_card) {
   switch (played_card.value) {
   case VAL_SKIP:
-    /*
-     * Следующий игрок пропускает ход
-     */
     game->current_player = next_player_index(game);
     break;
 
   case VAL_REVERSE:
-    /*
-     * Меняем направление
-     * В UNO при 2 игроках reverse = skip
-     */
     game->direction_clockwise = !game->direction_clockwise;
 
     if (game->players_count == 2) {
@@ -63,16 +34,10 @@ void apply_card_effect(Game *game, Card played_card) {
     break;
 
   default:
-    /*
-     * Обычные карты — без эффекта
-     */
     break;
   }
 }
 
-/*
- * Переключение хода
- */
 void advance_turn(Game *game) {
   game->players[game->current_player].is_turn = false;
   game->current_player = next_player_index(game);
@@ -120,58 +85,4 @@ Card draw_card(Deck *deck) {
     return deck->cards[deck->top++];
   }
   return (Card){COL_WILD, VAL_WILD};
-}
-
-const char *color_to_string(Color color) {
-  switch (color) {
-  case COL_RED:
-    return "red";
-  case COL_YELLOW:
-    return "yellow";
-  case COL_GREEN:
-    return "green";
-  case COL_BLUE:
-    return "blue";
-  case COL_WILD:
-    return "wild";
-  default:
-    return "unknown";
-  }
-}
-
-const char *value_to_string(Value value) {
-  switch (value) {
-  case VAL_0:
-    return "0";
-  case VAL_1:
-    return "1";
-  case VAL_2:
-    return "2";
-  case VAL_3:
-    return "3";
-  case VAL_4:
-    return "4";
-  case VAL_5:
-    return "5";
-  case VAL_6:
-    return "6";
-  case VAL_7:
-    return "7";
-  case VAL_8:
-    return "8";
-  case VAL_9:
-    return "9";
-  case VAL_SKIP:
-    return "skip";
-  case VAL_REVERSE:
-    return "reverse";
-  case VAL_DRAW_TWO:
-    return "draw_two";
-  case VAL_WILD:
-    return "wild";
-  case VAL_WILD_DRAW_FOUR:
-    return "wild_draw_four";
-  default:
-    return "unknown";
-  }
 }
--- source/uno_rules.h
+++ source/uno_rules.h
@@ -3,22 +3,12 @@
 
 #include "server.h"
 
-bool can_play_card(Card player_card, Color current_color, Card top_card);
-
+// Функции, специфичные для сервера
 void apply_card_effect(Game *game, Card played_card);
-
 void advance_turn(Game *game);
-
 int next_player_index(Game *game);
-
 void create_uno_deck(Deck *deck);
-
 void shuffle_deck(Deck *deck);
-
 Card draw_card(Deck *deck);
-
-const char *color_to_string(Color color);
-
-const char *value_to_string(Value value);
 
 #endif
